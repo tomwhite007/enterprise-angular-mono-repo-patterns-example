b491424961b8e86cdf99e11eb6d7776a36dcc218
Tom White <tomwhite007@gmail.com>
2020-04-17 13:32:18 +0100

    added check-in component route

---
+++ libs/check-in/check-in-feature-shell/src/lib/check-in/check-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shell-check-in',
+  templateUrl: './check-in.component.html',
+  styleUrls: ['./check-in.component.scss']
+})
+export class CheckInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
