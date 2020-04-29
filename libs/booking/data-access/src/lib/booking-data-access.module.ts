5a5ab95efa8b349e0713b67dafefdb60b98b2c57
Tom White <tomwhite007@gmail.com>
2020-04-29 14:50:14 +0100

    illegal imports

--- libs/booking/data-access/src/lib/booking-data-access.module.ts
+++ libs/booking/data-access/src/lib/booking-data-access.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedDataAccessModule } from '@patterns-example/shared/data-access';
+import { CheckInDataAccessModule } from '../../../../check-in/data-access/src';
+import { CheckInFeatureShellModule } from '@patterns-example/check-in/check-in-feature-shell';
 
 @NgModule({
-  imports: [CommonModule, SharedDataAccessModule]
+  imports: [
+    CommonModule,
+    SharedDataAccessModule,
+    CheckInDataAccessModule,
+    CheckInFeatureShellModule
+  ]
 })
 export class BookingDataAccessModule {}
