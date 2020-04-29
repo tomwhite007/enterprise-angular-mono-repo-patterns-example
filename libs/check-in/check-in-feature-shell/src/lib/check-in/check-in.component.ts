8ee1495240c5a5bfd9ed9a7870e88f184be902cb
Tom White <tomwhite007@gmail.com>
2020-04-29 13:58:57 +0100

    fix lint errros

--- libs/check-in/check-in-feature-shell/src/lib/check-in/check-in.component.ts
+++ libs/check-in/check-in-feature-shell/src/lib/check-in/check-in.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'shell-check-in',
+  selector: 'check-in',
   templateUrl: './check-in.component.html',
   styleUrls: ['./check-in.component.scss']
 })
 export class CheckInComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
