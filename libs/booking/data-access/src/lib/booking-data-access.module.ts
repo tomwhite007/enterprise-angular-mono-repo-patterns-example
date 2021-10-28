4894d2d9a8951e46fd8d56139c10aa5fc890dafd
Tom White <tom_contract@Nessy.com>
2021-10-28 09:00:17 +0100

    fix: import path

--- libs/booking/data-access/src/lib/booking-data-access.module.ts
+++ libs/booking/data-access/src/lib/booking-data-access.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedDataAccessModule } from '@patterns-example/shared/data-access';
 import { CheckInDataAccessModule } from '../../../../check-in/data-access/src';
-import { CheckInFeatureShellModule } from '@patterns-example/check-in/check-in-feature-shell';
+import { CheckInFeatureShellModule } from '@patterns-example/check-in/feature-shell';
 
 @NgModule({
   imports: [
