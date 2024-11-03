4862afbcc2f5cb8b10b414afa3075d43764a1f40
Tom White <tomwhite007@gmail.com>
2024-11-03 21:13:06 +0000

    fix: add linkage to SharedSeatmapDataAccess

--- libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
+++ libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BookingDataAccessModule } from '@patterns-example/booking/data-access';
 import {
   BookingFeatureFlightSearchModule,
-  FlightSearchComponent
+  FlightSearchComponent,
 } from '@patterns-example/booking/feature-flight-search';
-import { BookingDataAccessModule } from '@patterns-example/booking/data-access';
 
 @NgModule({
   imports: [
@@ -17,24 +17,24 @@ import { BookingDataAccessModule } from '@patterns-example/booking/data-access';
       {
         path: '',
         component: FlightSearchComponent,
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'booking-feature-passenger-info',
         loadChildren: () =>
           import('@patterns-example/booking/feature-passenger-info').then(
-            module => module.BookingFeaturePassengerInfoModule
-          )
+            (module) => module.BookingFeaturePassengerInfoModule
+          ),
       },
       {
         path: 'shared-seatmap-feature-seat-listing',
         loadChildren: () =>
           import('@patterns-example/shared/seatmap/feature-seat-listing').then(
-            module => module.SharedSeatmapFeatureSeatListingModule
-          )
-      }
+            (module) => module.SharedSeatmapFeatureSeatListingModule
+          ),
+      },
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ])
-  ]
+    ]),
+  ],
 })
 export class BookingFeatureShellModule {}
--- libs/shared/seatmap/feature-seat-listing/src/lib/shared-seatmap-feature-seat-listing.module.ts
+++ libs/shared/seatmap/feature-seat-listing/src/lib/shared-seatmap-feature-seat-listing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedSeatmapDataAccessModule } from '@patterns-example/shared/seatmap/data-access';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    SharedSeatmapDataAccessModule,
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ])
-  ]
+    ]),
+  ],
 })
 export class SharedSeatmapFeatureSeatListingModule {}
